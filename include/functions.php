51ecd4ef8ed6014e669ee08eb137f50db457c154
danydacosta <danydc@comac.ch>
2017-10-20 16:18:53 +0200

    Refactoring

--- include/functions.php
+++ include/functions.php
@@ -6,14 +6,15 @@
         $stmt = $dbh->query("INSERT INTO {$table} ({$columns}) VALUES ({$values})");
     }
     
+    //Supprime les données d'une table et reset l'auto_increment
     function ClearTable($dbh, $table){
         $stmt = $dbh->query("DELETE FROM {$table}");
         $stmt = $dbh->query("ALTER TABLE {$table} AUTO_INCREMENT = 1");
     }
 
-    //Retourne une array des regions
-    function FetchRegionsFromDatabase($dbh){
-        $stmt = $dbh->query('SELECT * FROM tbl_regions');
+    //Retourne une array des enregistrements pour une table donnée
+    function FetchTableFromDatabase($dbh, $table){
+        $stmt = $dbh->query("SELECT * FROM {$table}");
         $stmt->execute();
 
         return $stmt->fetchAll();
