42d29baf227aa0a599a981c843ad9f9a5b93af03
danydacosta <danydc@comac.ch>
2017-10-20 14:32:40 +0200

    Ajout de la fonction permettant de rechercher les lignes d'une region

--- include/functions.php
+++ include/functions.php
@@ -1,2 +1,14 @@
 <?php
-    
+    function FetchLines($region){        
+        $doc = new simple_html_dom();
+        $doc->load(file_get_contents('http://www.transn.ch/reseau-horaires/',$region));
+
+        $array = array();
+        foreach($doc->find('area[class=fancyboxmap]') as $element){
+            $line = $element->getAttribute('ad-l');
+            if(end($array) !== $line){
+                array_push($array, $line);
+            }
+        }
+        return $array;
+    }
