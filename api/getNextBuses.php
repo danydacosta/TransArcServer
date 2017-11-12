b18f6fedd3cb266ad41b5c4b6f8c36512cc1aaae
danydc@comac.ch <danydc@comac.ch>
2017-11-12 11:20:54 +0100

    Vérification des paramètres

--- api/getNextBuses.php
+++ api/getNextBuses.php
@@ -1,2 +1,9 @@
 <?php
-    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-type: application/json');
+
+    include_once('../scripts/functions.php');
+
+    if(isset($_GET['origin']) && isset($_GET['destination']) && isset($_GET['line'])){
+        
+    }
